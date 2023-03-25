4c78b427f61e9294a8a27f67e92eb3950c6b45f3
carlos <orellano428@gmail.com>
2023-03-24 19:04:47 -0500

    config db

---
+++ database/db.ts
@@ -0,0 +1,44 @@
+import mongoose, { connections } from "mongoose";
+
+/** 
+ * 0:disconnected
+ * 1=conected
+ * 2 = conecting
+ * 3: disconecting
+ * 
+ */
+
+const mongooConnection ={
+    isConnected:0
+}
+
+export const connect = async ()=>{
+    if(mongooConnection.isConnected){
+        console.log('conect')
+        return;
+    }
+
+    if(mongoose.connections.length > 0){
+        mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+        if(mongooConnection.isConnected === 1){
+            console.log('usando conexion anterios')
+            return;
+        }
+                        await mongoose.disconnect();
+    }
+
+    await mongoose.connect('....');
+    mongooConnection.isConnected = 1;
+    console.log('conectado a mongo','...')
+
+
+}
+
+export const disconnect =async ()=>{
+    if(mongooConnection.isConnected !== 0)return
+    await mongoose.disconnect();
+
+    console.log('desconectado mondocb')
+
+}
